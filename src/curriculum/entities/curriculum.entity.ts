1ad73ba8e15fac86094f8d118c4a390173e34f9d
Felipe Gabriel <felipegabriel1106@outlook.com>
2024-11-27 08:21:18 -0300

    feat(curriculum): create curriculum on project

---
+++ src/curriculum/entities/curriculum.entity.ts
@@ -0,0 +1,11 @@
+export class Curriculum {
+    id: number;
+    name: string;
+    telephone: string;
+    email: string;
+    objective: string;
+    pro_experience: string;
+    back_academic: string;
+    skills: string;
+    references: string;
+}
